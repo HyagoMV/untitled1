f0edeaa2003da5a6de9c6b5313752c6d94a1a3da
Hyago <hyagomvale@gmail.com>
2021-11-25 19:47:07 -0300

    Adicionado exmplos com cast

--- src/main/java/wrapper_class/BoxingAndUnboxing.java
+++ src/main/java/wrapper_class/BoxingAndUnboxing.java
@@ -22,5 +22,26 @@ public class BoxingAndUnboxing {
         double s = d.doubleValue();         // Unboxing     - Develop
         double ss = dd;                     // AutoUnboxing - Compiler
 
+        double s1   = (double) Double.valueOf(10).intValue();   // Explicit cast int to double
+        double s2   = Double.valueOf(10).intValue();            // Implicit cast int to double
+
+        // -----------------------------------------------------
+
+        double dd0 = Integer.valueOf(0);                    // Integer to double with implicit cast
+        double dd1 = (double) Integer.valueOf(0);           // Integer to double with explicit cast
+
+        // Double dd2 = 0;                                  // int to Double no implicit cast - COMPILE ERROR
+        Double dd2 = (double) 0;                            // int to Double with explicit cast
+
+        // int ii0 =  Double.valueOf(0);                    // Double to int with implicit cast - COMPILE ERROR
+        // int ii0 = (int) Double.valueOf(0);               // Double to int with explicit cast - COMPILE ERROR
+
+        int ii0 = (int) (double) Double.valueOf(0);         // Double to double AND double to int with TWO explicit cast
+        int ii1 = (int) Double.valueOf(0).doubleValue();    // Double to double no cast AND double to int with explicit cast
+
+        // Integer ii0 = 0D;                                // double to Integer with implicit cast - COMPILE ERROR
+        Integer ii2 = (int) 0D;                             // double to Integer with explicit cast
+
+        Integer ii3 = Double.valueOf(0).intValue();         // Double to Integer no cast
     }
 }
